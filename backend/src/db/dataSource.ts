d65ea608c636a257f1af017f9fd7304c10ab2e4e
gyulabodor <gyulabodor95@gmail.com>
2022-10-26 15:41:38 +0200

    Changed database to PostgreSQL

--- backend/src/db/dataSource.ts
+++ backend/src/db/dataSource.ts
@@ -1,14 +1,17 @@
 import { DataSource } from "typeorm";
 import { DB_HOST, DB_NAME, DB_PASSOWRD, DB_PORT, DB_USERNAME } from "../configuration";
+import { Food } from "../entities/food";
+import { Friend } from "../entities/friend";
+
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
+    type: "postgres",
 	host: DB_HOST,
 	port: DB_PORT,
 	username: DB_USERNAME,
 	password: DB_PASSOWRD,
 	database: DB_NAME,
-	entities: ["../entities/*.js"],
+	entities: [Food,Friend],
 	synchronize: true,
 	logging: false,
 });
