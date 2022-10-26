8ad03480f183d7c16fde356d840e07378e23ea17
gyulabodor <gyulabodor95@gmail.com>
2022-10-26 15:53:31 +0200

    Created FoodService

---
+++ backend/src/services/foodService/foodService.ts
@@ -0,0 +1,22 @@
+import { FoodDTO } from "../../dtos/foodDTO";
+import { Food } from "../../entities/food"
+import { AppDataSource } from "../../db";
+import { UnprocessableEntityError } from "../../errors/UnprocessableEntityError";
+
+export const addNewFood = async (foodDTO: FoodDTO) :Promise<string> => {
+
+    const food = new Food();
+    food.name=foodDTO.name;
+
+    const foodCheck = await AppDataSource.getRepository(Food).findOneBy({
+        name: food.name,
+    });
+
+    if (foodCheck) {
+        throw new UnprocessableEntityError("Food already exists!");
+    }
+
+    await AppDataSource.getRepository(Food).save(food);
+
+    return "New food has been successfully saved!";
+}
